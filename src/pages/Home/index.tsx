9a25beeca0f6ef125f5fb9301a1764215ca95ca3
lbenjaminq <benjaminlbq12@gmail.com>
2022-11-11 09:54:12 -0300

    Feat(src/public): Change Icon

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,11 +2,15 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import { Landing } from '../../components/Landing'
 import ListProducts from '../../components/ListProducts/ListProducts'
+import style from "./Home.module.css"
 
 export const Home: React.FC<{}> = () => {
   return (
     <div>
       <Landing/>
+      <div className={style.gradient}>
+        <h1 style={{textAlign:"center",fontSize:"5rem"}}>SALES</h1>
+      </div>
       <ListProducts/>
     </div>
     )
